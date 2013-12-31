e004807dfc94a01e292a13829b1aa8383165e64f
Myeongjin <aranet100@gmail.com>
2014-01-01 08:16:52 +0900

    update messages and add korean translation

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -5,9 +5,6 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
-
 $lang['expired'] = 'Ihr Passwort ist abgelaufen. Bitte <a href="?do=resendpwd">fordern Sie ein neues an</a>.';
-
-
 
 //Setup VIM: ex: et ts=4 :
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -5,9 +5,6 @@
  * @author Andreas Gohr <andi@splitbrain.org>
  */
 
-
 $lang['expired'] = 'Your password expired. Please <a href="?do=resendpwd">request a new one</a>.';
-
-
 
 //Setup VIM: ex: et ts=4 :
